818b5af41ae1d0a76d91f68067857855118651e7
unknown <kadir.nisic@gmail.com>
2025-05-27 17:05:09 +0200

    added mongo

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,16 +1,35 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [msg, setMsg] = useState('')
+  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    axios.get('http://localhost:8000/api/hello')
-      .then(res => setMsg(res.data.message))
-      .catch(err => console.error(err))
-  }, [])
-
-  return <h1>{msg || 'Učitavam...'}</h1>
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
